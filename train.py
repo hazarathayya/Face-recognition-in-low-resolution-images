3c7f550486a71ae3beaac2b6e059fcd99632f42e
Hazarath <hazarathayyayallanki@gmail.com>
2025-02-03 10:42:10 +0530

    added model, train files

---
+++ train.py
@@ -0,0 +1,5 @@
+# Things to try:
+# 1. What happens if you use larger network?
+# 2. Better normalization with BatchNorm
+# 3. Different learning rate (is there a better one?)
+# 4. Change the architecture of CNN.
